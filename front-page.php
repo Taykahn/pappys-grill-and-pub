62f93e67452bff4f29d7b199ce2ccb82c431fea0
Taykahn <marshallc428@gmail.com>
2017-07-19 16:16:25 -0500

    first commit

--- front-page.php
+++ front-page.php
@@ -8,18 +8,23 @@
 get_header() ?>
 
 	<!-- Section One -->
+
 	<?php do_action( 'about_bar' ); ?>
 
 	<!-- Section Two -->
+
 	<?php do_action( 'featured_specials' ); ?>
 
 	<!-- Section Three -->
+
 	<?php do_action( 'gc_co_rp' ); ?>
 
 	<!-- Section Four -->
+
 	<?php do_action( 'map_location' ); ?>
 
 	<!-- Section Five -->
+
 	<?php do_action( 'slider' ); ?>
 
 <?php get_footer() ?>
